b5a8ee4b65733909b92dff9a4bae76534a5254bc
machine <analyticrye@gmail.com>
2020-06-20 15:44:37 -0400

    fetch method for User added

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
 import { User } from "./models/User";
-import axios from "axios";
-const user = new User({ name: "Ryan", age: 38 });
 
-// axios.post(`http://localhost:3000/users`, {
-//   name: "myName",
-//   age: 20,
-// });
+const user = new User({ id: 1 });
 
-axios.get(`http://localhost:3000/users/1`);
+user.fetch();
