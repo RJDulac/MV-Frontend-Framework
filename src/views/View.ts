92e3ce4d86f70ba6d58d4fe45f026918df5bb2f0
machine <analyticrye@gmail.com>
2020-06-24 21:42:43 -0400

    region map system implemented

--- src/views/View.ts
+++ src/views/View.ts
@@ -1,12 +1,18 @@
 import { Model } from "../models/Model";
 
 export abstract class View<T extends Model<K>, K> {
+  regions: { [key: string]: Element } = {};
   constructor(public parent: Element, public model: T) {
     this.bindModel();
   }
-  abstract eventsMap(): { [key: string]: () => void };
-  abstract template(): string;
 
+  abstract template(): string;
+  regionsMap(): { [key: string]: string } {
+    return {};
+  }
+  eventsMap(): { [key: string]: () => void } {
+    return {};
+  }
   bindModel(): void {
     this.model.on("change", () => {
       this.render();
@@ -22,12 +28,23 @@ export abstract class View<T extends Model<K>, K> {
       });
     }
   }
+  mapRegions(fragement: DocumentFragment): void {
+    const regionsMap = this.regionsMap();
+    for (let key in regionsMap) {
+      const selector = regionsMap[key];
+      const element = fragement.querySelector(selector);
+      if (element) {
+        this.regions[key] = element;
+      }
+    }
+  }
   render(): void {
     this.parent.innerHTML = "";
     const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template();
 
     this.bindEvents(templateElement.content);
+    this.mapRegions(templateElement.content);
     this.parent.append(templateElement.content);
   }
 }
